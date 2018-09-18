1bf39516693d078ecf9357924f5aea23df99d2e9
Igor Filippenko <asm.xor.eax.eax@gmail.com>
2018-09-19 02:43:29 +0300

    Drag and drop component added

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h1>{{msg}}</h1>
+      <div 
+        v-on:mousedown="onDragStart($event)" 
+        v-on:mousemove="onDrag($event)"
+        v-on:mouseup="onDragStop($event)"
+        class="my-drag"
+        v-bind:style="{'left': left, 'top': top}"></div>
     <map-table></map-table>
   </div>
 </template>
@@ -15,12 +21,35 @@ export default {
   },
   data () {
     return {
-      msg: 'Fuel map'
+      msg: 'Fuel map',
+      left: 0,
+      top: 0,
+      clicked: false
+    }
+  },
+  methods: {
+    onDragStart: event => {
+      this.clicked = true
+    },
+    onDrag: event => {
+      console.log(this.clicked)
+      if (this.clicked) {
+        console.log(event)
+        this.left = event.clientX
+      }
+    },
+    onDragStop: event => {
+      this.clicked = false
     }
   }
 }
 </script>
 
 <style scoped>
-
+.my-drag {
+  width: 100px;
+  height: 100px;
+  background: gray;
+  position: absolute;
+}
 </style>
